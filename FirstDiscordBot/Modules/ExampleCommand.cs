ed6c519e121f46d18c82d9acfa0b09df164b38f6
Carlos Lopez <=>
2021-03-17 15:00:36 -0400

    Add project files.

---
+++ FirstDiscordBot/Modules/ExampleCommand.cs
@@ -0,0 +1,81 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstDiscordBot.Modules
+{
+    public class ExampleCommand : ModuleBase
+    {
+        [Command("hello")]
+        public async Task HelloCommand()
+        {
+            var sb = new StringBuilder();
+
+            var user = Context.User;
+
+            sb.AppendLine("You are " + user.Username);
+        
+            var avatarURL = user.GetAvatarUrl();
+            if(avatarURL == null)
+            {
+                sb.AppendLine("You really need to update your profile picture.");
+            }
+            else
+            {
+                sb.AppendLine("Your avatar image is located: " + user.GetAvatarUrl());
+            }
+            
+
+            await ReplyAsync(sb.ToString());
+        }
+
+        [Command("time")]
+        public async Task GetCurrentTime()
+        {
+            var now = DateTime.Now.ToString("yyyy-MM-dd HH:mm tt");
+
+            await ReplyAsync("The current time is now: " + now);
+        }
+
+        [Command("8ball")]
+        [Alias("ask")]
+        public async Task AskEightBall([Remainder]string args = null)
+        {
+            var reply = new StringBuilder();
+            var replies = new List<string>();
+
+            replies.Add("Yes");
+            replies.Add("No");
+            replies.Add("Maybe");
+            replies.Add("Wouldn't you like to know...");
+
+            if(args == null || !args.Contains("?"))
+            {
+                reply.AppendLine("Sorry, we can't answer a question you didn't ask.");
+            }
+            else
+            {
+                var question = args;
+
+                int randomIndex = new Random().Next(replies.Count);
+                var answer = replies[randomIndex];
+
+                if (question.Contains("is real"))
+                {
+                    answer = "I think the better question is, are you real?";
+                }else if (question.Contains("what"))
+                {
+                    answer = "Great question, can you define, what?";
+                }
+
+                reply.AppendLine(answer);
+            }
+
+            await ReplyAsync(reply.ToString());
+        }
+
+    }
+}
---
+++ FirstDiscordBot/Program.cs
@@ -0,0 +1,93 @@
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using FirstDiscordBot.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstDiscordBot
+{
+    public class Program
+    {
+        private readonly IConfiguration _config;
+        private DiscordSocketClient _client;
+        public Program()
+        {
+            // create the configuration
+            var _builder = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile(path: "config.json");
+
+            // build the configuration and assign to _config          
+            _config = _builder.Build();
+        }
+
+        public static void Main(string[] args)
+        {
+            new Program().MainAsync().GetAwaiter().GetResult();
+
+        }
+
+        public async Task MainAsync()
+        {
+            using (var services = ConfigureServices())
+            {
+                var client = services.GetRequiredService<DiscordSocketClient>();
+                _client = client;
+
+                // setup logging and the ready event
+                client.Log += LogAsync;
+                client.Ready += ReadyAsync;
+                services.GetRequiredService<CommandService>().Log += LogAsync;
+
+                await client.LoginAsync(TokenType.Bot, _config["Token"]);
+                await client.StartAsync();
+
+                await services.GetRequiredService<CommandHandler>().InitializeAsync();
+
+                await Task.Delay(-1);
+            }
+
+        }
+
+        private Task LogAsync(LogMessage log)
+        {
+            Console.WriteLine(log.ToString());
+            return Task.CompletedTask;
+        }
+
+        private Task ReadyAsync()
+        {
+            Console.WriteLine($"Connected as -> [] :)");
+            return Task.CompletedTask;
+        }
+
+        private ServiceProvider ConfigureServices()
+        {
+            // this returns a ServiceProvider that is used later to call for those services
+            // we can add types we have access to here, hence adding the new using statement:
+            // using csharpi.Services;
+            // the config we build is also added, which comes in handy for setting the command prefix!
+            return new ServiceCollection()
+                .AddSingleton(_config)
+                .AddSingleton<DiscordSocketClient>()
+                .AddSingleton<CommandService>()
+                .AddSingleton<CommandHandler>()
+                .BuildServiceProvider();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
